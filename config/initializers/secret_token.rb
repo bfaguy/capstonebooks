399f33e375296a140103a681cd8afbe5b04e561e
guy cao <guycao@gmail.com>
2014-05-20 12:47:05 -0700

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capstonebooks::Application.config.secret_key_base = '92f5df14ec265ffcc3e4281c26f9dda9575adfa77d7e460e0c48f47fdca3130c8305f9c5a5e783b80d7cc467d314d836a21a682e0ef3762b385c03e336106d2d'
